1d0a5fd1d44e1e8e790ff1071e59cb5e45e3e35b
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2025-02-03 16:34:51 -0300

    Criando o metodo de INSERT ao banco de dados

--- pos-java-jdbc/src/main/java/dao/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -1,15 +1,50 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaojdbc.SingleConnection;
+import model.Userposjava;
 
 public class UserPosDAO {
 
 	private Connection connection;
-	
-	
+		
 	public UserPosDAO() {
 		connection = SingleConnection.getConnection();
+		
+	}
+	
+	public void salvar(Userposjava userposjava) {
+		try {
+			//Instrução SQL.
+			String sql = "insert into userposjava (id, nome, email) values (?,?,?)";
+			
+			//PreparedStatement é o metodo que faz o insert sql.
+			PreparedStatement insert = connection.prepareStatement(sql);
+			
+			//No insert primeiro informamos a posição e depois o valor do campo, a posição é refetente a sequencia no insert into acima.
+			insert.setLong(1, userposjava.getId());
+			insert.setString(2, userposjava.getNome());
+			insert.setString(3, userposjava.getEmail());			
+			//execute pega o sql prepara ele atraves dos parametros(posição, valor) passados e executa.
+			insert.execute();
+			//salva os dados no banco.
+			connection.commit();
+			
+		} catch (Exception e) {
+			try {
+				//rollback() se der algum erro reverte a operação no banco de dados.
+				connection.rollback();
+				
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+				
+			}
+			
+			e.printStackTrace();
+			
+		}		
 	}
 }
--- pos-java-jdbc/src/test/java/testeconexaojdbc/TesteConexaoBancoJdbc.java
+++ pos-java-jdbc/src/test/java/testeconexaojdbc/TesteConexaoBancoJdbc.java
@@ -2,14 +2,25 @@ package testeconexaojdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteConexaoBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		//SingleConnection.getConnection();
 		
+		//DAO
+		UserPosDAO userPosDAO = new UserPosDAO();
+		//Model
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(6L);
+		userposjava.setNome("paulo");
+		userposjava.setEmail("paulo@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
 	}
 	
 }
