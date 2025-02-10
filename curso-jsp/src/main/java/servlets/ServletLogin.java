dbfd0dcb635071f9d519f42a79365496802c5ebf
Bruno Wellington <brunocarvalho.desenv@gmail.com>
2025-02-10 15:25:46 -0300

    Estruturando os packages do projeto JSP e criando o modelo de login

--- curso-jsp/src/main/java/servlets/ServletLogin.java
+++ curso-jsp/src/main/java/servlets/ServletLogin.java
@@ -1,10 +1,13 @@
 package servlets;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
+
 import java.io.IOException;
 
 @WebServlet("/ServletLogin") //Mapeamento de URL que vem da tela
@@ -20,12 +23,31 @@ public class ServletLogin extends HttpServlet {
 
 	}
 
+	
 	//Recebe os dados enviados por um formulario
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println(request.getParameter("nome"));
-		System.out.println(request.getParameter("idade"));
+		//Recebendo os dados de login e senha vindos do front e armazenando em variaveis
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
 		
+		//Validando se os dados de login e senha foram informados
+		if(login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+			
+			//Setando login e senha na classe ModelLogin
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+		}else {
+			//Redirecionando o usuario para o index caso nao passe na validação 
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			//Passando mensagem ao usuario caso nao passe na validação 
+			request.setAttribute("msg", "Informe o login e senha corretamente!");
+			//Comando de redirecionamento
+			redirecionar.forward(request, response);
+		}
+			
 	}
 
 }
--- curso-jsp/src/main/webapp/index.jsp
+++ curso-jsp/src/main/webapp/index.jsp
@@ -7,15 +7,33 @@
 	<title>Curso JSP</title>
 </head>
 <body>
-	<h2>Formulario</h2>
+	<h2>Bem Vindo ao Curso de JSP</h2>
 	
-	<form action="ServletLogin" method="post">
-	
-		<input name="nome">		
-		<input name="idade">
-		
-		<input type="submit" value="Enviar">
+	<form action="ServletLogin" method="post">	
+		<table>
+			<tr>
+				<td><label>Login</label></td>
+				<td>
+					<input name="login" type="text">
+				</td>
+			</tr>
+			
+			<tr>
+				<td><label>Senha</label></td>
+				<td>
+					<input name="senha" type="password">
+				</td>	
+			</tr>
+			
+			<tr>
+				<td></td>
+				<td>
+					<input type="submit" value="Entrar">
+				</td>		
+			</tr>
+		</table>	
 	</form>
-
+		<!-- Passando menssagem caso o login e senha estejam nulos ou em branco -->
+		<span>${msg}</span>			
 </body>
 </html>
